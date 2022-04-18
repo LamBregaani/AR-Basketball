4de9c016e55ec59f8e2d9d0a3985096f0a1738de
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-04-18 05:37:46 -0400

    Added a script to print to the console via Unity events

---
+++ Assets/Scripts/PrintDebug.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintDebug : MonoBehaviour
+{
+    public void Print(string _message)
+    {
+        Debug.Log(_message);
+    }
+}
