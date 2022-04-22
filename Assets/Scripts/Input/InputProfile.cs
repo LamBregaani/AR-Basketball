4a7c5072b9bea53939c5b137dfe08f4ad262c6a9
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-04-22 13:38:46 -0400

    Added class for input profiles
    Input profiles are a set of controls and settings for different states of the the game(UI, Main, etc.)

---
+++ Assets/Scripts/Input/InputProfile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class InputProfile
+{
+    public string name;
+
+    public abstract void Update();
+}
