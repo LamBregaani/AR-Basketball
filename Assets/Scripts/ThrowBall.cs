01c40e96fcf85b8acb69631c7ce14a0be806c66d
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-04-22 13:41:41 -0400

    Added class to fire a basketball
    A basket ball is fired when the user swipes down on the screen
    The force applied to the ball when fired is relative to the distance of the swipe
    An angle is also applied based on the angle of the swipe

---
+++ Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    [Tooltip("The maximum amount of force the ball can be thrown at")]
+    [SerializeField] private float m_maxThrowForce;
+
+    [Tooltip("The time bewteen being able to throw the ball")]
+    [SerializeField] private float m_throwDelay;
+
+    [Tooltip("the point where the ball is fired from")]
+    [SerializeField] private Transform m_throwPosition;
+
+    [Tooltip("The camera or object to get the forward vector from")]
+    [SerializeField] private GameObject m_camera;
+
+    //Call for getting a ball from a pool
+    private BallSupplier m_ballSupplier;
+
+    //Refernce to the main input profile
+    private MainInput m_input;
+
+    //Used for the throw delay
+    private bool m_canThrow = true;
+
+    private void Awake()
+    {
+        m_ballSupplier = GetComponent<BallSupplier>();
+    }
+
+    private void Start()
+    {
+        m_input = InputController.instance.inputProfiles[MainInput.profileName] as MainInput;
+
+        m_input.onSwipeEnded.AddListener(Throw);
+    }
+
+    private void OnEnable()
+    {
+        m_input.onSwipeEnded.AddListener(Throw);
+    }
+
+    private void OnDisable()
+    {
+        m_input.onSwipeEnded.RemoveListener(Throw);
+    }
+
+
+
+    public void Throw(Swipe _swipeData, float _percentage)
+    {
+        
+        if (!m_canThrow)
+            return;
+
+        //Gets a new object from the pool
+        var ball = m_ballSupplier.GetNewBall();
+
+        //Changes the object;s postion to the throw position
+        ball.transform.position = m_throwPosition.position;
+
+        //Used to slighty angle the direction the ball is thrown based on the normalized direction of the swipe
+        var angle = _swipeData.direction2D.normalized;
+
+        //Set the direction the ball will be thrown
+        Vector3 direction = new Vector3(m_camera.transform.forward.x + angle.x, m_camera.transform.forward.y, m_camera.transform.forward.z);
+
+        m_canThrow = false;
+
+        ball.AddForce(direction * _percentage * m_maxThrowForce, ForceMode.Impulse);
+
+        StartCoroutine(ThrowDelay());
+    }
+
+    /// <summary>
+    /// The delay between being able to fire a ball
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ThrowDelay()
+    {
+        yield return new WaitForSeconds(m_throwDelay);
+        m_canThrow = true;
+    }
+}
+
+
