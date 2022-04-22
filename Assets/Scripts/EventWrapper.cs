673ba8893c7a3eb0e22f392f1eef6aa4c008ea42
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-04-22 13:37:21 -0400

    Added ability for events that pass a generic type parameter

--- Assets/Scripts/EventWrapper.cs
+++ Assets/Scripts/EventWrapper.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Class used to create a joint C# and Unity event
+/// </summary>
 [System.Serializable]
 public class EventWrapper : UnityEvent
 {
     public delegate void OnEventInvoked();
 
     public event OnEventInvoked onEventInvoked;
-
-    public delegate void OnEventInvokedParam(object val);
-
-    public event OnEventInvokedParam onEventInvokedParam;
 
     public EventWrapper onEventInvokedUnity;
 
@@ -27,13 +26,46 @@ public class EventWrapper : UnityEvent
     }
 
     /// <summary>
-    /// Invoke both the Unity event and the C# event with a parameter
+    /// Add a listener to the C# event
     /// </summary>
-    public void InvokeEvent(object val)
+    /// <param name="_listener"></param>
+    public void AddListener(OnEventInvoked _listener)
     {
-        onEventInvokedParam?.Invoke(val);
+        onEventInvoked += _listener;
+    }
 
+    /// <summary>
+    /// Remove a listener to the C# event
+    /// </summary>
+    /// <param name="_listener"></param>
+    public void RemoveListener(OnEventInvoked _listener)
+    {
+        onEventInvoked -= _listener;
+    }
+
+
+}
+
+/// <summary>
+/// Create an event that takes parameter T
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[System.Serializable]
+public class EventWrapper<T> : UnityEvent
+{
+    public delegate void OnEventInvoked(T val);
+
+    public event OnEventInvoked onEventInvoked;
+
+    public EventWrapper<T> onEventInvokedUnity;
+
+    /// <summary>
+    /// Invoke both the Unity event and the C# event
+    /// </summary>
+    public void InvokeEvent(T val)
+    { 
         Invoke();
+        onEventInvoked.Invoke(val);
     }
 
     /// <summary>
@@ -46,14 +78,55 @@ public class EventWrapper : UnityEvent
     }
 
     /// <summary>
-    /// Add a listener to the C# event with a parameter
+    /// Remove a listener to the C# event
     /// </summary>
     /// <param name="_listener"></param>
-    public void AddListener(OnEventInvokedParam _listener)
+    public void RemoveListener(OnEventInvoked _listener)
     {
-        onEventInvokedParam += _listener;
+        onEventInvoked -= _listener;
     }
 
+}
+
+/// <summary>
+/// Used to make an event wrapper with two type paramaters
+/// </summary>
+/// <typeparam name="T1"></typeparam>
+/// <typeparam name="T2"></typeparam>
+public class EventWrapper<T1, T2> : UnityEvent
+{
+    public delegate void OnEventInvoked(T1 val, T2 val2);
+
+    public event OnEventInvoked onEventInvoked;
+
+    public EventWrapper<T1> onEventInvokedUnity;
+
+    /// <summary>
+    /// Invoke both the Unity event and the C# event
+    /// </summary>
+    public void InvokeEvent(T1 val, T2 val2)
+    {
+        Invoke();
+        onEventInvoked.Invoke(val, val2);
+    }
+
+    /// <summary>
+    /// Add a listener to the C# event
+    /// </summary>
+    /// <param name="_listener"></param>
+    public void AddListener(OnEventInvoked _listener)
+    {
+        onEventInvoked += _listener;
+    }
+
+    /// <summary>
+    /// Remove a listener to the C# event
+    /// </summary>
+    /// <param name="_listener"></param>
+    public void RemoveListener(OnEventInvoked _listener)
+    {
+        onEventInvoked -= _listener;
+    }
 
 }
 
