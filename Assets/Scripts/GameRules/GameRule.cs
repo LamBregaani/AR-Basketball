69b4d80b5e26f5623b9390cd216f621072538d48
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-06-22 00:57:42 -0400

    Added Gamerule script with a few base methods

---
+++ Assets/Scripts/GameRules/GameRule.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameRule : ScriptableObject
+{
+    public virtual void Init(GameModeController controller)
+    {
+
+    }
+
+    public virtual void StartRule()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void ResetRule()
+    {
+
+    }
+}
