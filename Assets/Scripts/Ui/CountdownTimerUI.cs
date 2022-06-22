aa75b0814b4e5b3cfa58b8c605602df2f0a88648
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-06-22 01:02:32 -0400

    Began work on UI script for countdowns

---
+++ Assets/Scripts/Ui/CountdownTimerUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownTimerUI : MonoBehaviour
+{
+    
+    [SerializeField] private float visableCountdownStart;
+
+    private float m_countdownTime;
+
+    private float m_remainingTime;
+}
