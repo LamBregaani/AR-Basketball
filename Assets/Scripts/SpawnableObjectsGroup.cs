d01f37e7c67000f6cb7182b71cd3f77cccc66ad9
Randall Forehand <forehar@patriots.cf.edu>
2022-06-26 19:53:37 -0400

    Added GetObject() method
    GetObject returns a random object from the group based on the spawn rates of the objects

--- Assets/Scripts/SpawnableObjectsGroup.cs
+++ Assets/Scripts/SpawnableObjectsGroup.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("SpawnableObjectsGroupDrawer")]
+#endif
 [System.Serializable]
+
+
 public class SpawnableObjectsGroup<T>
 {
     [SerializeField] public T[] objects;
@@ -13,5 +19,30 @@ public class SpawnableObjectsGroup<T>
     [SerializeField] public float[] objectSpawnRates = new float[0];
 
     public float[] ObjectSpawnRates { get => objectSpawnRates; set => objectSpawnRates = value; }
+
+    public T GetObject()
+    {
+
+        var randomVal = Random.Range(0.0001f, 100f);
+
+        var spawnRate = ObjectSpawnRates[0];
+
+        for (int i = 0; i < ObjectSpawnRates.Length; i++)
+        {
+            if (spawnRate > randomVal)
+            {
+                return Objects[i];
+                Debug.Log($"Random Value = {randomVal} Spawning: #{i}");
+                break;
+            }
+            else
+            {
+                spawnRate += ObjectSpawnRates[i];
+            }
+        }
+
+        Debug.Log($"Random Value = {randomVal}");
+        return Objects[0];
+    }
 
 }
