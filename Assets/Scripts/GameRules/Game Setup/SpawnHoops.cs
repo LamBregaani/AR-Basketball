d2a9c6c7fbb503e96c08ca2a2f80f74105249e6d
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2022-06-27 01:58:36 -0400

    Changed class name and sperated Hoops class

---
+++ Assets/Scripts/GameRules/Game Setup/SpawnHoops.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+
+[ExecuteAlways]
+[CreateAssetMenu(menuName = "Game Rules/Spawn Hoops", fileName = "New Hoop Properties")]
+
+public class SpawnHoops : GameRule
+{
+    public override void Init(GameModeController controller)
+    {
+        HoopBuilder.Create(hoopsGroup.GetObject(), Vector3.zero);
+    }
+
+    [SerializeField] public SpawnableObjectsGroup<Hoop> hoopsGroup;
+
+    [HideInInspector]public const string filePath = "Assets/Resources/Game Properties/Default Hoop Builder Properties.asset";
+
+#if UNITY_EDITOR
+    public static SpawnHoops GetOrCreateDefaultInstance()
+    {
+        var properties = AssetDatabase.LoadAssetAtPath<SpawnHoops>(filePath);
+
+        if (properties == null)
+        {
+            properties = CreateInstance<SpawnHoops>();
+
+            AssetDatabase.CreateAsset(properties, filePath);
+
+            AssetDatabase.SaveAssets();
+        }
+
+        return properties;
+    }
+# endif
+}
+
+
+
+
